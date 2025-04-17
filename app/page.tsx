8f2fc96588d857053bbaa4abc6c0e122eb39a640
Aayushgoyal00 <aayush04.ag@gmail.com>
2025-04-17 16:07:39 +0530

    Comment out Overshoot Day paragraph in Home component for future reference

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
           <h2 className="text-xl uppercase tracking-wider">What is your</h2>
           <h1 className="text-6xl font-bold">Ecological Footprint?</h1>
           <p className="text-xl mt-4">How many planets do we need if everybody lives like you?</p>
-          <p className="text-xl">When is your personal Overshoot Day?</p>
+          {/* <p className="text-xl">When is your personal Overshoot Day?</p> */}
         </div>
 
         <div className="mt-16">
